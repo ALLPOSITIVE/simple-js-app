e60e3daf40ee38dd75a5dd950236f01896fc64de
ALLPOSITIVE <58402165+ALLPOSITIVE@users.noreply.github.com>
2020-05-03 20:27:27 +0100

    creation of variables and data - the 1st step to create Pokedex app

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
-alert('Hello World');
-var favouriteFood = 'Beef Curry';
-document.write(favouriteFood);
+var pokemanList = [ Mons1, Mons2, Mons3] ;
+var Mons1 = {
+  name: 'Pikachu',
+  height: 0.2,
+  types: ['grass', 'poison']
+}
+var Mons2 = {
+  name: 'Raichu',
+  height: 0.8,
+  types: ['grass', 'poison']
+}
+var Mons3 = {
+  name: 'Nidorina',
+  height: 0.8,
+  types: ['grass', 'poison']
+}
