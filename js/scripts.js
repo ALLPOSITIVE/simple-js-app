687569cc7081e120a4cae319c7f42bac2bc32c5b
ALLPOSITIVE <58402165+ALLPOSITIVE@users.noreply.github.com>
2020-05-09 11:42:39 +0100

    added conditionals and loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,23 @@
-var pokemanList = [ Mons1, Mons2, Mons3] ;
-var Mons1 = {
+var monS1 = {
   name: 'Pikachu',
   height: 0.2,
   types: ['grass', 'poison']
 }
-var Mons2 = {
+var monS2 = {
   name: 'Raichu',
   height: 0.8,
   types: ['grass', 'poison']
 }
-var Mons3 = {
+var monS3 = {
   name: 'Nidorina',
-  height: 0.8,
+  height: 1.8,
   types: ['grass', 'poison']
+}
+var pokemonList = [monS1, monS2, monS3];
+
+for (var i = 0, i< pokemonList.length; i++) {
+  document.write('<h2>' + pokemonList[i].name + '</h2>' + ' Height: ' + pokemonList[i].height)
+if (pokemonList[i].height >= 1.0) {
+  document.write(' (Wow that\'s big!)' )
+  }
 }
